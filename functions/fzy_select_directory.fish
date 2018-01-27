b69e4cbfde59245380cd697ef6369e2706f59d0c
Dietrich Daroch <dietr1ch@acm.org>
2018-01-27 15:33:03 -0800

    select_directory: Drop use of locate
    It really is a hack over locate and may lead to stale results.
    `fd` does fine when running on $HOME.

--- functions/fzy_select_directory.fish
+++ functions/fzy_select_directory.fish
@@ -1,22 +1,6 @@
 
-function __fzy_get_directories_db -d 'Get a list of directories using locate'
-  # Locate lists all files, and directories without a '/' on the end :/
-  # 1. Get only entries starting with $PWD
-  # 2. Trim $PWD
-  # 3. Filter hidden files (and directories)
-  # 4. Trim the filename by dropping everything after the last /
-  # 5. Uniquify (locate output should be sorted)
-  command locate "$PWD" \
-    | command grep '^'"$PWD/"'.*/' \
-    | command sed 's#'$PWD'/##' \
-    | command grep -v -E "^\." \
-    | command sed 's#/[^/]*$##' \
-    | uniq
-end
-
-function __fzy_get_directories_raw -d 'Get a list of directories using fd or find'
+function __fzy_get_directory_list -d 'Get a list of directories using fd or find'
   if which fd >/dev/null ^/dev/null
-    echo "fd"
     command fd . -t d
   else
     command find . \( -path '*/\\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune  -o -type d -print ^/dev/null \
@@ -25,42 +9,8 @@ function __fzy_get_directories_raw -d 'Get a list of directories using fd or fin
   end
 end
 
-function __fzy_get_directories -d 'Get a list of directories'
-  set -l use_db 'yes'
-  if command git rev-parse --show-toplevel >/dev/null ^/dev/null
-    set use_db 'no'
-  else if which hg >/dev/null ^/dev/null and command hg root >/dev/null ^/dev/null
-    set use_db 'no'
-  else
-    set -l fs (df $PWD | tail -n+2 | awk '{print $1}')
-    switch $fs
-      case 'tmpfs'
-        set use_db 'no'
-      case '*'
-        set use_db 'yes'
-    end
-  end
-
-  if not which locate >/dev/null ^/dev/null
-    set use_db 'no'
-  end
-
-  set -q FZY_ENABLE_DB or set -l FZY_ENABLE_DB 'no'
-
-  if [ "$FZY_ENABLE_DB" != 'yes' ]
-    set use_db 'no'
-  end
-
-  switch $use_db
-    case 'yes'
-      __fzy_get_directories_db
-    case '*'
-      __fzy_get_directories_raw
-  end
-end
-
 function fzy_select_directory -d 'cd to a directory using fzy'
-  __fzy_get_directories | fzy | read -l foo
+  __fzy_get_directory_list | fzy | read -l foo
   if [ $foo ]
     cd $foo
   end
